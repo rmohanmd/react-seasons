0708d8841b5dc42658910d91e0824f20230b7b55
rmohanmd <rmohan.md@gmail.com>
2022-08-28 21:04:01 -0400

    Added constructor() and defined state

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// const App = () => {
-//   window.navigator.geolocation.getCurrentPosition(
-//     (position) => console.log(position.coords.latitude),
-//     (err) => console.log(err)
-//   );
-//   return <div>Latitude: </div>;
-// };
-
 class App extends React.Component {
+  //Another function we can use but isn't required for react constructor() (a javascript function)
+  //constructor function is first to be called so it's great to use to initiate state
+  constructor(props) {
+    //have to use this first, because React.Compenent class has it's own necessary constructor but using our own
+    //constructor stops React.Component from using it's contructor funciton. Thus we use super(props) to
+    //also run the React.Component contructor()
+    super(props); //calls parents constructor function
+
+    this.state = { lat: null };
+  }
+
+  //React says we have to define render!!!
   render() {
-    return <div>Latitude:</div>;
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position.coords.latitude),
+      (err) => console.log(err)
+    );
+    return <div>Latitude: {this.state.lat}</div>;
   }
 }
 
