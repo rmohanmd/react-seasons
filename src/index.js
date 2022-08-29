e246b99206762fba3480303a5ca3a83b0c08ac4d
rmohanmd <rmohan.md@gmail.com>
2022-08-28 23:50:51 -0400

    replaced constructor function with single line of code

--- src/index.js
+++ src/index.js
@@ -2,18 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-  //Another function we can use but isn't required for react constructor() (a javascript function)
-  //constructor function is first to be called so it's great to use to initiate state
-  constructor(props) {
-    //have to use this first, because React.Compenent class has it's own necessary constructor but using our own
-    //constructor stops React.Component from using it's contructor funciton. Thus we use super(props) to
-    //also run the React.Component contructor()
-    super(props); //calls parents constructor function
-
-    this.state = { lat: null, errorMessage: "" }; //this is the only time we directly assign anything to this.state
-    //moved this from render() because render() will be called so often, keeping this code in there will
-    //bog down the system
-  }
+  state = { lat: null, errorMessage: "" };
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => this.setState({ lat: position.coords.latitude }),
